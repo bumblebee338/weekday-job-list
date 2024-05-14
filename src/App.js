cdc3678cfabe6116f2c3353f739c9e0c84b2f3ab
Bumblebee <rahulomnitrics@gmail.com>
2024-05-14 15:05:42 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import JobList from './components/JobList';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Job Portal</h1>
+      <JobList />
+    </div>
+  );
+}
+
+export default App;
